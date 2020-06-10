6ccb889f25d442f0d9fd14c42a92f3ef4202f536
G. Jason Head <gjhead@gmail.com>
2020-06-10 17:35:37 -0400

    base site

--- a/js/script.js
+++ a/js/script.js
@@ -3,46 +3,6 @@ function example() {
 	
 }
 
-// breakpoints
-
-$(window).setBreakpoints({
-// use only largest available vs use all available
-	distinct: true, 
-// array of widths in pixels where breakpoints
-// should be triggered
-	breakpoints: [
-		320,
-		480,
-		768,
-		1024
-	] 
-});		
-
-$(window).bind('enterBreakpoint320',function() {
-	...
-});
-
-$(window).bind('exitBreakpoint320',function() {
-	...
-});
-
-$(window).bind('enterBreakpoint768',function() {
-	...
-});
-
-$(window).bind('exitBreakpoint768',function() {
-	...
-});
-
-
-$(window).bind('enterBreakpoint1024',function() {
-	...
-});
-
-$(window).bind('exitBreakpoint1024',function() {
-	...
-});
-
 /* EXECUTE JAVASCRIPT */
 
 /* On Document Ready */
